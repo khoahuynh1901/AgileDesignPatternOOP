9c67dc6e4cd9a0d848afd89c4372d5aa4b6b3fc6
khoa huynh <khoahuynh1901@gmail.com>
2024-03-29 11:28:01 -0400

    updated unit test

---
+++ UnitTest/Bank(17March)/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_17March_
+{
+    public class BankAccount
+    {
+        public string Owner { get; }
+        public decimal Balance { get; private set; }
+
+        public BankAccount(string owner, decimal initialBalance)
+        {
+            Owner = owner;
+            Balance = initialBalance;
+        }
+
+        public void Credit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount must be greater than or equal to zero.");
+            }
+
+            Balance += amount;
+        }
+    }
+}
